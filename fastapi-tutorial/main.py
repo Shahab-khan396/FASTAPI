829a9d3729eb323038d25d6ced2c6e66d7e4f743
Shahab-khan396 <ksab2922@gmail.com>
2026-02-22 00:47:15 +0500

    practicing get method

--- fastapi-tutorial/main.py
+++ fastapi-tutorial/main.py
@@ -1,11 +1,23 @@
+import json
+
 from fastapi import FastAPI
 
 app= FastAPI()
 
+def load_data():
+    with open("pateint.json", "r") as f:
+        data = json.load(f)
+    return data
+
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "patient Management System api"}
 
 @app.get("/about")
 def about():
-    return {"message": "This is a FastAPI tutorial"}
+    return {"message": "A fully Functional patient management system api to manage your patients records"}
+
+@app.get("/view")
+def view():
+    data=load_data()
+    return data
